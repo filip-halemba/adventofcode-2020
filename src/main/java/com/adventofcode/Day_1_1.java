2f54231afb110ba8cf331d0ffb2bd30a0173a87b
Filip Halemba <filip.halembaa@gmail.com>
2020-12-24 20:35:36 +0100

    Day 1 improvements

--- src/main/java/com/adventofcode/Day_1_1.java
+++ src/main/java/com/adventofcode/Day_1_1.java
@@ -8,22 +8,18 @@ class Day_1_1 {
 
     private final static int EXPECTED_SUM = 2020;
 
-    private final Set<Integer> set;
-    private final List<Integer> list;
+    private final Set<Integer> input;
 
 
     Day_1_1(List<Integer> input) {
-        this.list = input;
-        this.set = new HashSet<>(list.size());
+        this.input = new HashSet<>(input);
     }
 
     Long solve() {
-        for (int current : list) {
+        for (int current : input) {
             int lookFor = EXPECTED_SUM - current;
-            if (set.contains(lookFor)) {
+            if (input.contains(lookFor)) {
                 return (long) (current * lookFor);
-            } else {
-                set.add(current);
             }
         }
         return null;
